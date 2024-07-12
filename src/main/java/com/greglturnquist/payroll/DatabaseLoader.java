5adbfcd6186de95448986e526a34e661fca8bebf
Zlelik <zlelik2000@gmail.com>
2024-07-12 23:15:48 +0200

    Changed server port and simplify the password for easier testing.

--- src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
+++ src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
@@ -22,18 +22,16 @@ public class DatabaseLoader implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception {
 
-		Manager greg = this.managers.save(new Manager("greg", "turnquist", "ROLE_MANAGER"));
-		Manager oliver = this.managers.save(new Manager("oliver", "gierke", "ROLE_MANAGER"));
+		Manager greg = this.managers.save(new Manager("greg", "1", "ROLE_MANAGER"));
+		Manager oliver = this.managers.save(new Manager("oliver", "1", "ROLE_MANAGER"));
 
-		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("greg", "turnquist",
-				AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
+		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("greg", "1", AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
 
 		this.employees.save(new Employee("Frodo", "Baggins", "ring bearer", greg));
 		this.employees.save(new Employee("Bilbo", "Baggins", "burglar", greg));
 		this.employees.save(new Employee("Gandalf", "the Grey", "wizard", greg));
 
-		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("oliver", "gierke",
-				AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
+		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("oliver", "1", AuthorityUtils.createAuthorityList("ROLE_MANAGER")));
 
 		this.employees.save(new Employee("Samwise", "Gamgee", "gardener", oliver));
 		this.employees.save(new Employee("Merry", "Brandybuck", "pony rider", oliver));
